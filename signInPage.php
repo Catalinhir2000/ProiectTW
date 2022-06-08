24494ee72ee4eb6b663ed3e74dbbe66eec6af09c
Cătălin Sîrcu <75885223+catalin-alexandru-s@users.noreply.github.com>
2022-06-08 19:23:45 +0300

    modified signup form

--- signInPage.php
+++ signInPage.php
@@ -1,9 +1,13 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
     <title>CyMaT</title>
-    <link rel="stylesheet" href="css pages/signInPage.css">
+    <link rel="stylesheet" href="csspages/signInPage.css">
     <link rel="shortcut icon" type="image/png" href="images/screwdriver-wrench-solid.svg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,15 +29,15 @@
             <div class="input-boxes">
               <div class="input-box">
                 <i class="fas fa-user"></i>
-                <input type="text" name = "username" placeholder="Enter your username" required>
+                <input type="text" name = "uidLogin" placeholder="Enter your username" required>
               </div>
               <div class="input-box">
                 <i class="fas fa-lock"></i>
-                <input type="password" name = "pwd" placeholder="Enter your password" required>
+                <input type="password" name = "pwdLogin" placeholder="Enter your password" required>
               </div>
               <div class="text"><a href="mainPage.php">Return to home?</a></div>
               <div class="button input-box">
-                <input type="submit" name = "submit" value="Log In">
+                <input type="submit" name = "submitLogin" value="Log In">
               </div>
               <div class="text sign-up-text">Don't have an account? <label for="flip">Signup now</label></div>
             </div>
@@ -68,6 +72,34 @@
               </div>
               <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
             </div>
+            <?php
+              if(isset($_GET["error"])){
+                  if($_GET["error"]=="passwordNotMatching"){
+                      echo "<p style = 'color:red'>Passwords not matching!</p>";
+                  }
+                  else if($_GET["error"]=="invalidUsername"){
+                      echo "<p style = 'color:red'>Invalid username!</p>";
+                  }
+                  else if($_GET["error"]=="usernameTaken"){
+                      echo "<p style = 'color:red'>Username already taken, think of another one!</p>";
+                  }
+                  else if($_GET["error"]=="invalidEmail"){
+                      echo "<p style = 'color:red'>Enter a valid email adress!</p>";
+                  }
+                  else if($_GET["error"]=="statementFailed"){
+                      echo "<p style = 'color:red'>Something went wrong, try again!</p>";
+                  }
+                  else if($_GET["error"]=="emptyInput"){
+                    echo "<p style = 'color:red'>Fill in the required fields!</p>";
+                  }
+                  else if($_GET["error"] == "shortPasswordLength"){
+                    echo "<p style = 'color:red'>Your password should be above 6 characters!</p>";
+                  }
+                  else if($_GET["error"]=="none"){
+                      echo "<p>You have signed up!</p>";
+                  }
+                }
+              ?>
       </form>
     </div>
     </div>
