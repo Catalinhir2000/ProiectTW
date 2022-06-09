886d0a01bf6cb2a4dfa8aca80b820b0368d172d3
Catalinhir2000 <75785900+Catalinhir2000@users.noreply.github.com>
2022-06-09 19:30:48 +0300

    Main page for users and admin

--- mainPage.php
+++ mainPage.php
@@ -1,10 +1,14 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang = "en">
   <head>
     <meta charset="utf-8">
     <title>CyMaT</title>
     <link rel="shortcut icon" type="image/png" href="images/screwdriver-wrench-solid.svg">
-    <link rel="stylesheet" type="text/css" href="css pages/mainPage.css">
+    <link rel="stylesheet" type="text/css" href="csspages/mainPage.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,22 +17,56 @@
   </head>
   <body>
     <nav>
-      <input type ="checkbox" id="check">
-      <label for = "check" class="checkbutton">
-        <i class="fas fa-solid fa-bars"></i>
-      </label>
+        <input type ="checkbox" id="check">
+        <label for = "check" class="checkbutton">
+          <i class="fas fa-solid fa-bars"></i>
+        </label>
       <label class = "logo">Cy<span>MaT</span></label>
       <ul>
         <li><a class ="active" href ="mainPage.php">Home</a></li>
-        <li><a href ="bookingPage.php">Booking</a></li>
         <li><a href ="stockPage.php">Stock</a></li>
-        <li><a href ="signInPage.php">Log In</a></li>
+        <?php
+            if(isset($_SESSION["useruid"])){
+              if($_SESSION["userid"] == 1){
+                echo "<li><a href='appointments.php'>Appointments</a></li>";
+                echo "<li><a href='postItemPage.php'>Post</a></li>";
+                echo "<li><a href='../ProiectTw/includes/logout.inc.php'>Log out</a></li>";
+              }
+              else if($_SESSION["userid"] != 1){
+                echo "<li><a href ='bookingPage.php'>Booking</a></li>";
+                echo "<li><a href='profilePage.php'>Profile</a></li>";
+                echo "<li><a href='inbox.php'>Inbox</a></li>";
+                echo "<li><a href='../ProiectTw/includes/logout.inc.php'>Log out</a></li>";
+              }
+            }
+            else{
+                echo"<li><a href='signInPage.php'>Log In</a></li>";
+          }    
+            ?>
       </ul>
     </nav>
-    <section id = "showcase">
-         <h2>Welcome to CyMaT</h2>
-         <h4>To get started book a session with us</h4>
-         <a href ="bookingPage.php" class = "button">Book Now</a>
-  </section>
+    <?php
+    if(isset($_SESSION["useruid"])){
+              if($_SESSION["userid"] == 1){
+                echo "<section id = 'showcase'>
+                <h2>Welcome Admin</h2>
+                </section>";
+              }
+              else if($_SESSION["userid"] != 1){
+               echo "<section id = 'showcase'>
+                <h2>Welcome to CyMaT</h2>
+                <h4>To get started book a session with us</h4>
+                <a href = 'bookingPage.php' class = 'button'>Book Now</a>
+                </section>";
+              }
+            }
+            else{
+              echo "<section id = 'showcase'>
+                <h2>Welcome to CyMaT</h2>
+                <h4>In order to book, you have to log in.</h4>
+                <a href = 'signInPage.php' class = 'button'>Log In</a>
+                </section>";
+          }
+  ?>
   </body>
 </html>
